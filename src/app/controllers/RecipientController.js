109d51836b5c825831bfb6bdabad509f201f3429
Marcelo <carbono.marcelo@gmail.com>
2020-02-04 22:01:04 -0300

    RecipientController adjustments

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -23,6 +23,19 @@ class RecipientController {
 
     const { name, number, zip_code } = req.body;
     let { address_complement } = req.body;
+
+    const apiResponse = await axios.get(
+      `http://ws.matheuscastiglioni.com.br/ws/cep/find/${zip_code}/json`
+    );
+
+    const address = apiResponse.data.logradouro;
+
+    if (address_complement == '')
+      address_complement = apiResponse.data.complemento;
+
+    const state = apiResponse.data.estado;
+    const city = apiResponse.data.cidade;
+
     const recipientExists = await Recipient.findOne({
       where: { name, address, number, city }
     });
@@ -32,18 +45,6 @@ class RecipientController {
     }
 
     if (!recipientExists) {
-      const apiResponse = await axios.get(
-        `http://ws.matheuscastiglioni.com.br/ws/cep/find/${zip_code}/json`
-      );
-
-      const address = apiResponse.data.logradouro;
-
-      if (address_complement == '')
-        address_complement = apiResponse.data.complemento;
-
-      const state = apiResponse.data.estado;
-      const city = apiResponse.data.cidade;
-
       const recipient = await Recipient.create({
         name,
         address,
@@ -59,16 +60,10 @@ class RecipientController {
 
   async update(req, res) {
     const schema = yup.object().shape({
-      name: yup.string().required(),
-      number: yup
-        .string()
-        .required()
-        .matches(/[0-9]+/),
+      name: yup.string(),
+      number: yup.string().matches(/[0-9]+/),
       address_complement: yup.string().matches(/[A-Za-z]*/),
-      zip_code: yup
-        .string()
-        .required()
-        .matches(/[0-9]{8}/)
+      zip_code: yup.string().matches(/[0-9]{8}/)
     });
 
     if (!(await schema.isValid(req.body))) {
