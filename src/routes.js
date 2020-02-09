bae3d8df71ba3a71d2b23210d3a7181685bdf273
Marcelo <carbono.marcelo@gmail.com>
2020-02-09 15:31:18 -0300

    table deliverymans ok

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,4 +21,5 @@ routes.put('/recipients/:id', RecipientController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
+routes.post('/deliveryman', DeliverymanController.store);
 module.exports = routes;
